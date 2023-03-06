1a86de63f9aee2a064dac87b407e2ff01b2dce5b
unknown <jake@Allmight>
2023-03-06 21:24:54 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// var navLinks = document.querySelectorAll('.nav-link');
+
+// navLinks.forEach(function(link) {
+
+//   link.addEventListener('click', function() {
+//     navLinks.forEach(function(link) {
+//       link.classList.remove('active');
+//     });
+//     link.classList.add('active');
+//   });
+// });
+
+
+window.onscroll = function() {
+
+    var navbar = document.querySelector(".navbar");
+    var active = document.querySelector(".navbar-collapse");
+
+    if (window.pageYOffset > 100) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");      
+    }
+    active.classList.remove("show");
+  };
+
+
+const li = document.querySelectorAll(".nav-link");
+const sec = document.querySelectorAll("section");
+
+function activemenu() { 
+    let len=sec.length;
+    
+    while(--len && window.scrollY + 97 < sec[len].offsetTop){}
+        li.forEach(ltx => ltx.classList.remove("active"));
+        li[len].classList.add("active")
+ }
+
+ activemenu();
+ window.addEventListener("scroll",activemenu);
+  
