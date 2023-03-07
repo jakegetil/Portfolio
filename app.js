e659efb7900124665be1ee796949ce90aabc004b
unknown <jake@Allmight>
2023-03-07 18:34:14 +0800

    march 7 2023

--- app.js
+++ app.js
@@ -38,4 +38,22 @@ function activemenu() {
 
  activemenu();
  window.addEventListener("scroll",activemenu);
-  
+
+
+
+ const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if(entry.isIntersecting){
+        entry.target.classList.add('show');
+      }
+      else{
+        entry.target.classList.remove('show');
+      }
+    });
+ });
+
+ const hidden = document.querySelectorAll('.hidden');
+ hidden.forEach((el) => observer.observe(el) );
+
+ 
